f5b762a5066a2b7b5e5cdb9bf1120ba40ee8cc0d
daiana romero <daianaar94@gmail.com>
2022-01-26 13:27:55 -0300

    primeros cambios

---
+++ src/main/java/ar/edu/davinci/dvds20211cRomeroDaiana/DvDs20211cRomeroDaianaApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20211cRomeroDaiana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20211cRomeroDaianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20211cRomeroDaianaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dvds20211cRomeroDaiana/DvDs20211cRomeroDaianaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20211cRomeroDaiana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20211cRomeroDaianaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
