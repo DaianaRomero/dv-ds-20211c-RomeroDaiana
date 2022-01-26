915db19ef633f55c926909ab975f87510bc8c8c0
daiana romero <daianaar94@gmail.com>
2022-01-26 15:58:12 -0300

    cambios finales

---
+++ src/main/java/ar/edu/davinci/dvds20211cRomeroDaiana/controller/request/ItemInsertRequest.java
@@ -0,0 +1,20 @@
+package ar.edu.davinci.dvds20211cRomeroDaiana.controller.request;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ItemInsertRequest {
+
+	private Integer cantidad;
+
+	private Long prendaId;
+
+}
+
